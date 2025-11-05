c229a2e6b3ae66509031882bae6e9d12a8847af8
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-11-05 09:38:10 -0800

    corrected the jobs model

--- models/job.js
+++ models/job.js
@@ -1,73 +1,73 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-        },
-        phone: {
-            type: String,
-        },
-        lastContacted: {
-            type: Date,
-        },
-        linkedin: {
-            type: String,
-        },
-    }
-)
+const contactSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+	},
+	phone: {
+		type: String,
+	},
+	lastContacted: {
+		type: Date,
+	},
+	linkedin: {
+		type: String,
+	},
+});
 
-const jobSchema = new mongoose.Schema(
-    {
-        jobTitle: {
-            type: String,
-            required: true,
-        },
-        companyName: {
-            type: String,
-            required: true,
-        },
-        companyWebsite: {
-            type: String,
-            required: true,
-        },
-        companyLinkedin: {
-            type: String,
-            required: true,
-        },
-        jobDescription: {
-            type: String,
-            required: true,
-        },
-        dateApplied: {
-            type: Date,
-            required: true,
-        },
-        referral: {
-            type: String,
-            required: true,
-        },
-        status: {
-            type: String,
-            required: true,
-            enum: ['interested', 'applied', 'rejected', 'pending', 'interviewing', 'ghosted']
-        },
-        contacts: [contactSchema],
+const jobSchema = new mongoose.Schema({
+	jobTitle: {
+		type: String,
+		required: true,
+	},
+	companyName: {
+		type: String,
+		required: true,
+	},
+	companyWebsite: {
+		type: String,
+		required: true,
+	},
+	companyLinkedIn: {
+		type: String,
+	},
+	jobDescription: {
+		type: String,
+		required: true,
+	},
+	dateApplied: {
+		type: Date,
+		required: true,
+	},
+	referral: {
+		type: String,
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: [
+			"interested",
+			"applied",
+			"rejected",
+			"pending",
+			"interviewing",
+			"ghosted",
+		],
+	},
+	contacts: [contactSchema],
 
-        notes: {
-            type: String,
-            required: true,
-        },
-    }
-)
+	notes: {
+		type: String,
+	},
+});
 
-const Job = mongoose.model("Job", jobSchema)
-modules.exports = Job;
+const Job = mongoose.model("Job", jobSchema);
+module.exports = Job;
