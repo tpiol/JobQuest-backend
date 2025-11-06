9bc475b8408994487f79868d7372e6d7faf70c5f
p-tigris <shoumik_dhar@yahoo.com>
2025-11-06 01:43:07 -0500

    contact routes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
 const Job = require("../models/job.js");
+const { verify } = require("jsonwebtoken");
 const router = express.Router();
 
 // POST ROUTES
@@ -69,5 +70,63 @@ router.delete("/:jobId", verifyToken, async (req, res) => {
         res.status(500).json({ err: err.message });
     }
 })
+
+// Contact routes
+// POST routes
+router.post("/:jobId/contacts", verifyToken, async (req, res) => {
+	try {
+		const job = await Job.findById(req.params.jobId);
+
+		if (!job.seeker.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+		job.contacts.push(req.body);
+		await job.save();
+
+		const newContact = job.contacts[job.contacts.length - 1];
+
+		res.status(201).json({ newContact: newContact, message: "Contact created successfully" });
+	} catch (err) {
+		res.status(500).json({ err: err.message });
+	}
+});
+
+// PUT routes
+router.put("/:jobId/contacts/:contactId", verifyToken, async (req, res) => {
+	try {
+		const job = await Job.findById(req.params.jobId);
+		const contact = job.contacts.id(req.params.contactId)
+		
+		if (!job.seeker.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+		Object.assign(contact, req.body);
+
+		await job.save();
+		res.status(200).json({ message: "Contact updated successfully" });
+	} catch (err) {
+		res.status(500).json({ err: err.message });
+	}
+
+});
+
+// DELETE routes
+router.delete("/:jobId/contacts/:contactId", verifyToken, async (req, res) => {
+	try {
+		const job = await Job.findById(req.params.jobId);
+
+		if (!job.seeker.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+		job.contacts.remove({ _id: req.params.contactId });
+		await job.save();
+		res.status(200).json({ message: "Contact deleted successfully" });
+	} catch(err) {
+		res.status(500).json({ err: err.message });
+	}
+});
 
 module.exports = router;
